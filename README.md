4d9520d32261d8a715435a8402c240bc60f35a8b
Kartik Nagappa <kartiknagappa@gmail.com>
2016-12-12 16:38:54 -0800

    Initial checkin with express, bootstrap, and PostgreSQL support.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fannie Mae and Freddie Mac Loan Performance Analysis
+
+## Setup
+
+### [Localhost](http://localhost:5000/)
+1. clone this repo
+2. npm install
+3. npm start
+
+### Heroku
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/js/'));
+app.use('/modules', express.static(__dirname + '/node_modules/'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
